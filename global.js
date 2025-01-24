c60c32dcc9536c38ed19c8edc56c21a501a91b1e
PandaFalls2004 <129922943+PandaFalls2004@users.noreply.github.com>
2025-01-24 11:48:57 -0800

    step 3

--- global.js
+++ global.js
@@ -4,10 +4,26 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks=$$("nav a");
+// let navLinks=$$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
 // second option   
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' }, 
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/PandaFalls2004', title: 'Github' }
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // TODO create link and add it to nav
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
