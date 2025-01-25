833ba459a9e615d0012c950b8c7b90dcf623e882
PandaFalls2004 <129922943+PandaFalls2004@users.noreply.github.com>
2025-01-24 23:31:47 -0800

    steps

--- global.js
+++ global.js
@@ -26,6 +26,7 @@ for (let p of pages) {
     let url = p.url;
     let title = p.title;
     // TODO create link and add it to nav
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
     if (!ARE_WE_HOME && !url.startsWith('http')) {
         url = '../' + url;
       }
